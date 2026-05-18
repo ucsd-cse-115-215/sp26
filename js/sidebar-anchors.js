dc86ecdedef7dc2d0cd27f6c0e18eb480fe39fc1
jpolitz <jpolitz@users.noreply.github.com>
2026-05-18 09:02:38 +0000

    deploy: ucsd-cse-115-215/notes@304dad77742d4fedac88139fa9fd4790d283eff0

--- js/sidebar-anchors.js
+++ js/sidebar-anchors.js
@@ -14,6 +14,7 @@
       ["From CSE29 Homework to Concurrent Allocator", "experiment-setup"],
       ["Doing my Homework", "r0-baseline"],
       ["A First Try at Concurrency", "r1-concurrent"],
+      ["Concurrency: Asking for Speed", "r2-concurrent-pushed"],
     ],
   };
 
